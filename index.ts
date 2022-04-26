2d7b1e20350b44ff0cf8b7c1c8344bd45243d4f0
e <abrandecarlo@protonmail.com>
2022-04-26 16:56:28 -0700

    .

--- index.ts
+++ index.ts
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
   res.send('Rainbow NFT API');
 });
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+
+
 app.get('/currentTokenId', currentTokendId_);
 app.get('/totalSupply', totalSupply_);
 app.get('/randomTokenURI', randomTokenURI_);
